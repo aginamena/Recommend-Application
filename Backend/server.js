61f7b3fc2cd8b0bb61e5199981a2e0c5e8b10bcc
aginamena5 <aginamena5@gmail.com>
2021-12-28 15:06:24 -0500

    added heroku postbuild

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 9000;
 const usersRoute = require("./routes/usersRoute");
 const bookRoute = require("./routes/bookRoute");
+const path = require("path");
 // console.log(usersRoute);
 
 //setting up middleware
@@ -12,6 +13,14 @@ const bookRoute = require("./routes/bookRoute");
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'Frontend/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'Frontend/build', 'index.html'));
+    });
+}
 
 //importing the database function and running it
 require("./config/dbconnect")();
